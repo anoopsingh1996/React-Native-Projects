47b59f06c00c1418ab44d553bbcb88506f09d43c
anoopsingh1996 <anoopraghuvansi@gmail.com>
2020-01-10 15:39:04 +0530

    TODO APP

---
+++ TODO/components/sandbox.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Text, View} from 'react-native';
+
+export default function Snadbox() {
+    return (
+        <View style={styles.container}>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+     container: {
+         paddingTop: 100,
+         backgroundColor: '#ddd',
+     }
+});
